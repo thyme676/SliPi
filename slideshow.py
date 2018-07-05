2ff157458ab0c998980bffd0cc3453e8211493a0
Mark <msw4@pdx.edu>
2018-07-05 15:02:02 -0700

    Outlined desired flow

---
+++ slideshow.py
@@ -0,0 +1,15 @@
+# slideshow.py
+
+# Pull config vars from slideshow.cfg file
+
+# Search directories
+
+
+# Initialize the slideshow
+
+
+# Loop the slideshow
+
+# Check for new slides
+
+# Cleanup
