bcf03a67b8ed9e4e29fd4a23632c746f40ab9d40
Mark <msw4@pdx.edu>
2018-07-26 16:02:57 -0700

    Demo not included yet.

--- slideshow.py
+++ slideshow.py
@@ -13,7 +13,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import time
 import pi3d
-import demo # If pi3d isn't installed
+#import demo # If pi3d isn't installed
 
 #####
 # Vars, will be loaded from config in the future.
