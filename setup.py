a7dc9a547c63a6ff9c3d2dfc4c49071171114c7d
Mark Williams <thyme676@users.noreply.github.com>
2019-05-07 18:07:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thyme676/SliPi",
-    packages=setuptools.find_packages(),
+    packages=['SliPi'],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
